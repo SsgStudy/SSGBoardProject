ecf38e4202bbe3bc19013ebe8fb237b69f7bd946
yunyeobin <sky_line22@naver.com>
2024-02-04 13:46:16 +0900

    feat : Getter,setter 추가

--- src/boardJdbc/Board.java
+++ src/boardJdbc/Board.java
@@ -1,5 +1,6 @@
 package boardJdbc;
 
+
 import lombok.Data;
 
 @Data
@@ -10,4 +11,65 @@ public class Board {
     private String bwriter;
     private String date;
 
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getBtitle() {
+        return btitle;
+    }
+
+    public void setBtitle(String btitle) {
+        this.btitle = btitle;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "bno=" + bno +
+                ", btitle='" + btitle + '\'' +
+                ", bcontent='" + bcontent + '\'' +
+                ", bwriter='" + bwriter + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+
+    public Board(int bno, String btitle, String bcontent, String bwriter, String date) {
+        this.bno = bno;
+        this.btitle = btitle;
+        this.bcontent = bcontent;
+        this.bwriter = bwriter;
+        this.date = date;
+    }
+
+    public Board() {
+    }
 }
