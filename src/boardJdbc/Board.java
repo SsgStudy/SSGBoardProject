f966dcd7fd9a32660666339ab03fb1c92ab40b33
장서윤 <seoy316@naver.com>
2024-02-04 16:29:18 +0900

    fix: getter setter 삭제

--- src/boardJdbc/Board.java
+++ src/boardJdbc/Board.java
@@ -3,53 +3,15 @@ package boardJdbc;
 
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 public class Board {
     private int bno;
     private String btitle;
     private String bcontent;
     private String bwriter;
-    private String date;
-
-    public int getBno() {
-        return bno;
-    }
-
-    public void setBno(int bno) {
-        this.bno = bno;
-    }
-
-    public String getBtitle() {
-        return btitle;
-    }
-
-    public void setBtitle(String btitle) {
-        this.btitle = btitle;
-    }
-
-    public String getBcontent() {
-        return bcontent;
-    }
-
-    public void setBcontent(String bcontent) {
-        this.bcontent = bcontent;
-    }
-
-    public String getBwriter() {
-        return bwriter;
-    }
-
-    public void setBwriter(String bwriter) {
-        this.bwriter = bwriter;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
+    private Date date;
 
     @Override
     public String toString() {
@@ -60,14 +22,6 @@ public class Board {
                 ", bwriter='" + bwriter + '\'' +
                 ", date='" + date + '\'' +
                 '}';
-    }
-
-    public Board(int bno, String btitle, String bcontent, String bwriter, String date) {
-        this.bno = bno;
-        this.btitle = btitle;
-        this.bcontent = bcontent;
-        this.bwriter = bwriter;
-        this.date = date;
     }
 
     public Board() {
