c97a845f7287668fd7a843c1d737c73e502a8af1
장서윤 <seoy316@naver.com>
2024-02-04 16:29:50 +0900

    fix: 게시글 작성 수정
    날짜 생성, 게시글 번호 생성 제거

--- src/boardJdbc/BoardDao.java
+++ src/boardJdbc/BoardDao.java
@@ -1,26 +1,91 @@
 package boardJdbc;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BoardDao {
 
     static Connection conn;
 
-    public BoardDao() throws SQLException, ClassNotFoundException {
-        conn =  DBConnection.getConnection();
+    public BoardDao() {
+        try {
+            conn = DBConnection.getConnection();
+        } catch (ClassNotFoundException c) {
+            c.printStackTrace();
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
     }
 
-    public void create() {
+    public void create(Board board) {
+        try {
+            String sql = "INSERT INTO boards (btitle, bcontent, bwriter) VALUES (?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, board.getBtitle());
+            pstmt.setString(2, board.getBcontent());
+            pstmt.setString(3, board.getBwriter());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void read() {
+    public List<Board> read() {
+        List<Board> boards = new ArrayList<>();
+
+        try{
+            String sql = new StringBuilder().append("SELECT * FROM boards").toString();
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                Board board = new Board();
+                board.setBno(rs.getInt("bno"));
+                board.setBtitle(rs.getString("btitle"));
+                board.setBcontent(rs.getString("bcontent"));
+                board.setBwriter(rs.getString("bwriter"));
+                board.setDate(rs.getDate("bdate"));
+                boards.add(board);
+            }
+
+            rs.close();
+            pstmt.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return boards;
+
     }
 
-    public void readOne(Integer bno) {
+    public Board readOne(Integer bno) {
+
+        Board board = new Board();
+        try{
+            String sql = new StringBuilder().append("SELECT * FROM boards WHERE bno=?").toString();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,bno);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                board.setBno(rs.getInt("bno"));
+                board.setBtitle(rs.getString("btitle"));
+                board.setBcontent(rs.getString("bcontent"));
+                board.setBwriter(rs.getString("bwriter"));
+                board.setDate(rs.getDate("bdate"));
+            }
+        }catch(SQLException s){
+            s.printStackTrace();
+        }
+
+        return board;
     }
 
     public int update(int bno, Board board) {
