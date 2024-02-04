fe5e62811556bd8245ccd0d2e2535f4db9dac3d2
장서윤 <seoy316@naver.com>
2024-02-04 16:34:11 +0900

    Revert "fix: getter setter 삭제"
    This reverts commit f966dcd7fd9a32660666339ab03fb1c92ab40b33.

--- src/boardJdbc/Board.java
+++ src/boardJdbc/Board.java
@@ -3,15 +3,53 @@ package boardJdbc;
 
 import lombok.Data;
 
-import java.sql.Date;
-
 @Data
 public class Board {
     private int bno;
     private String btitle;
     private String bcontent;
     private String bwriter;
-    private Date date;
+    private String date;
+
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getBtitle() {
+        return btitle;
+    }
+
+    public void setBtitle(String btitle) {
+        this.btitle = btitle;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
     @Override
     public String toString() {
@@ -22,6 +60,14 @@ public class Board {
                 ", bwriter='" + bwriter + '\'' +
                 ", date='" + date + '\'' +
                 '}';
+    }
+
+    public Board(int bno, String btitle, String bcontent, String bwriter, String date) {
+        this.bno = bno;
+        this.btitle = btitle;
+        this.bcontent = bcontent;
+        this.bwriter = bwriter;
+        this.date = date;
     }
 
     public Board() {
