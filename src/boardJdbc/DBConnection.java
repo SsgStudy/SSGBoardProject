7da5ddb918c2eede8fe1abd52c1d3128c8551b39
zzzjinwook <zzzjinwook@naver.com>
2024-02-04 16:21:40 +0900

    gitignore

--- src/boardJdbc/DBConnection.java
+++ src/boardJdbc/DBConnection.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+
     private static Connection conn;
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException
-    {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
 
-        if (conn == null)
+        if (conn == null || conn.isClosed() || !conn.isValid(1)) // 연결이 닫혀있거나 유효하지 않으면 새로운 연결 생성
+
         {
-            String url = "jdbc:mysql://27.96.130.40";
-            String user = "";
+            String url = "jdbc:mysql://27.96.130.40:3306/SSGSTRG";
+            String user = "zzzjinwook";
             String pw = "pw";
             Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -23,10 +24,8 @@ public class DBConnection {
         return conn;
     }
 
-    public static Connection getConnection(String url, String user, String pw) throws ClassNotFoundException, SQLException
-    {
-        if (conn == null)
-        {
+    public static Connection getConnection(String url, String user, String pw) throws ClassNotFoundException, SQLException {
+        if (conn == null) {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(url, user, pw);
         }
@@ -34,12 +33,11 @@ public class DBConnection {
         return conn;
     }
 
-    public static void close() throws SQLException
-    {
-        if (conn != null)
-        {
-            if (!conn.isClosed())
+    public static void close() throws SQLException {
+        if (conn != null) {
+            if (!conn.isClosed()) {
                 conn.close();
+            }
         }
 
         conn = null;
