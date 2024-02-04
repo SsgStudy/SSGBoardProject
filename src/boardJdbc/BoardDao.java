21bf90846b8b3f465c2d4d6851b24ebfbead53cb
JiniiW <ksjine2@gmail.com>
2024-02-04 16:55:42 +0900

    feat: 게시물 삭제 구현
    선택 게시물 삭제, 전체 게시물 삭제

--- src/boardJdbc/BoardDao.java
+++ src/boardJdbc/BoardDao.java
@@ -2,14 +2,23 @@ package boardJdbc;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class BoardDao {
 
     Connection conn;
-    public BoardDao() throws SQLException, ClassNotFoundException {
-        conn =  DBConnection.getConnection();
+
+    public BoardDao()  {
+        try {
+            conn = DBConnection.getConnection();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void create() {
@@ -18,15 +27,32 @@ public class BoardDao {
     public void read() {
     }
 
-    public void readOne(Integer bno) {
+    public void readOne(int bno) {
     }
 
-    public void update(Integer bno) {
+    public void update(int bno) {
     }
 
-    public void delete(int inputBno) {
+    public void delete(int bno) {
+        String sql = "DELETE FROM boards WHERE bno=?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bno);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clear() {
+        String sql = "DELETE FROM boards";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.execute();
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
