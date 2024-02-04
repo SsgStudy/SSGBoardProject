20588b7991d81b8835fa37db364b705f9f30a4ea
장서윤 <seoy316@naver.com>
2024-02-04 14:59:40 +0900

    feat: 게시글 수정 DB

--- src/boardJdbc/BoardDao.java
+++ src/boardJdbc/BoardDao.java
@@ -2,12 +2,14 @@ package boardJdbc;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
 public class BoardDao {
 
-    Connection conn;
+    static Connection conn;
+
     public BoardDao() throws SQLException, ClassNotFoundException {
         conn =  DBConnection.getConnection();
     }
@@ -21,7 +23,27 @@ public class BoardDao {
     public void readOne(Integer bno) {
     }
 
-    public void update(Integer bno) {
+    public int update(int bno, Board board) {
+        int row = 0;
+
+        String sql = new StringBuilder()
+                .append("UPDATE boards SET ")
+                .append("btitle=?,")
+                .append("bcontent=?")
+                .append("WHERE bno=?")
+                .toString();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, board.getBtitle());
+            pstmt.setString(2, board.getBcontent());
+            pstmt.setInt(3, bno);
+
+            row = pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
+        return row;
     }
 
     public void delete(int inputBno) {
